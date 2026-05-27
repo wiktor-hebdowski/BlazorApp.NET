d9a388c4e0497ef532ac3d5147520a33ec1e9e47
Wiktor Hebdowski <whebdowski@gmail.com>
2026-05-27 23:46:57 +0200

    Add files via upload

---
+++ BlazorApp/Program.cs
@@ -0,0 +1,39 @@
+using BlazorApp;
+using BlazorApp.Components;
+using Microsoft.AspNetCore.Authentication.Certificate;
+using Microsoft.Extensions.ML;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddPredictionEnginePool<Feeling.ModelInput, Feeling.ModelOutput>()
+    .FromFile("feeling.mlnet");
+
+builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme).AddCertificate();
+
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
